ddb8109885620b88c7da3bdaedcf9487f6153650
M Mansour <mansourmo@tutanota.com>
2021-07-01 07:39:35 +0200

    "added sliding menu feature"

--- src/tolstoy-bio.js
+++ src/tolstoy-bio.js
@@ -4,15 +4,17 @@ let pageNumber = 0
 // hold the content for these  pages 
 
 const pages = [
-     { text: "Beekeeper", background: "#050505", color: "#FAFAFA"},
-     { text: "Holy Fool", background: "#050505", color: "#FAFAFA" },
-     { text: "Novelist", background: "#050505", color: "#FAFAFA" },
-     { text: "Gambler", background: "#050505", color: "#FAFAFA" },
-     { text: "Curmudgeon", background: "#050505", color: "#FAFAFA" },
-     { text: "Narcissist", background: "#050505", color: "#FAFAFA" },
-     { text: "God", background: "#050505", color: "#FAFAFA" }
+    { text: "Elder, Apostate, and Tsar", background: "pink", color: "white" },
+    { text: "Dropout", background: "green", color: "white" },
+    { text: "Landowner, Gambler, Officer, Writer", background: "red", color: "white" },
+    { text: "Literary Duellist and Repentant Nobleman", background: "purple", color: "white" },
+    { text: "Husband, Beekeeper, and Epic Poet", background: "yellow", color: "white" },
+    { text: "Student, Teacher, Father", background: "blue", color: "white" },
+    { text: "Novelist", background: "coral", color: "white" },
+    { text: "Pilgrim, Nihilist, Muzhik", background: "orange", color: "white" },
+    { text: "Sectarian, Anarchist, Holy Fool", background: "grey", color: "white" },
+   
 ]
-
 
 //pick the relevant tags
 
@@ -20,45 +22,43 @@ const nextTag = document.querySelector("div.arrows svg.next")
 const previousTag = document.querySelector("div.arrows svg.prev")
 const outputTag = document.querySelector("section p")
 const randomTag = document.querySelector("div.arrows svg.random")
+const bodyTag = document.querySelector("body")
 
 //make a next function to increase the pageNumber
 const next = function () {
     pageNumber = pageNumber + 1
 
-    if (pageNumber > pages.length -1) {
+    if (pageNumber > pages.length - 1) {
         pageNumber = 0
     }
 
     updatePage()
 }
 
-const previous = function() {
-   pageNumber = pageNumber -1
-   
+const previous = function () {
+    pageNumber = pageNumber - 1
+
     if (pageNumber < 0) {
-        pageNumber = pages.length -1
+        pageNumber = pages.length - 1
     }
 
     updatePage()
 
 }
 
-const random = function() {
-    pageNumber = Math.floor(Math.random() *  pages.length)
+const random = function () {
+    pageNumber = Math.floor(Math.random() * pages.length)
 
     updatePage()
 }
 
-  
+
 
 //this will update the content & style of the section
 const updatePage = function () {
     outputTag.innerHTML = pages[pageNumber].text
-    outputTag.style.backgroundColor = "#050505"
-    outputTag.style.color = "#FFBFA8"
-    outputTag.style.textTransform = "uppercase"
-    outputTag.style.fontWeight = "500"
-    outputTag.style.letterSpacing = ".2rem"
+    bodyTag.style.backgroundColor = pages[pageNumber].background
+    outputTag.style.color = "black"
 }
 
 //on click of nextTag, run this
@@ -71,6 +71,25 @@ previousTag.addEventListener("click", function () {
     previous()
 })
 
-randomTag.addEventListener("click", function() {
+randomTag.addEventListener("click", function () {
     random()
+})
+
+// when a user presses a key, check for arrows left or right
+// and do next or prev correctly
+
+document.addEventListener("keyup", function (event) {
+    console.log("testing 123")
+
+    // if the key being pressed is ArrowRight
+    if (event.key == "ArrowRight") {
+        next()
+    }
+
+    // if the key being pressed is ArrowLeft
+    if (event.key == "ArrowLeft") {
+        previous()
+    }
+
+
 })
