49df439ad5761645145dd1c2ae1248737fbfe22f
M Mansour <mansourmo@tutanota.com>
2021-07-03 11:36:36 +0200

    finished slide JS & styling

--- src/slides.js
+++ src/slides.js
@@ -31,9 +31,17 @@ slideArea.addEventListener("click", function () {
 // in a random place
 slideArea.addEventListener("mouseover", function() {
     images.forEach(image => {
-        const x = 200 * Math.random()
-        const y = 100 * Math.random()
+        const x = 25 * (Math.floor(Math.random() * 5)) -50
+        const y = 25 * (Math.floor(Math.random() * 5)) -50
 
         image.style.transform = `translate(${x}px, ${y}px)`
+    })
+})
+
+
+// when I move my mouse away, put the images back
+slideArea.addEventListener("mouseout", function () {
+    images.forEach(image => {
+        image.style.transform =""
     })
 })
